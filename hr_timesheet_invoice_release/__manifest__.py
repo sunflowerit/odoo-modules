8098faef3e105dc5ab6bb8683c0642e3ff774001
kiplangatdan <kiplangatdan@gmail.com>
2017-10-03 19:39:33 +0300

    port hr_timesheet_invoice_release to 10.0

--- hr_timesheet_invoice_release/__manifest__.py
+++ hr_timesheet_invoice_release/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 {
     'name': 'Timesheet invoice release',
-    'version': '8.0.1.0.0',
+    'version': '10.0.1.0.0',
     'summary': 'Release timesheet lines after invoice was cancelled',
     'author': 'Sunflower IT',  
     'website': 'http://www.sunflowerweb.nl',
@@ -10,5 +10,5 @@
         'hr_timesheet_invoice'
     ],
     'data': ['account_invoice_view.xml'],
-    'installable': False,
+    'installable': True,
 }
