387e97b2b2ea544da3b18340030ba3d9a77296fa
Tom Blauwendraat <antiflu@gmail.com>
2018-01-08 14:29:31 +0100

    Support branding module Sunflower IT

--- support_sunflower/__openerp__.py
+++ support_sunflower/__openerp__.py
@@ -1,7 +1,7 @@
-# coding: utf-8
+# -*- encoding: utf-8 -*-
 {
     'name': 'Support Branding Sunflower IT',
-    'version': '8.0.8.0.0',
+    'version': '8.0',
     'category': 'Support',
     'license': 'AGPL-3',
     'summary': 'Support branding for Sunflower IT',
