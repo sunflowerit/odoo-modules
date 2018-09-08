b7770977500a09659611854214a126d062a84894
KKamaa <kevkamaa96@gmail.com>
2018-09-08 19:45:35 +0300

    [ADD] float_time_seconds, support_freshfilter, testserver

---
+++ testserver/__manifest__.py
@@ -0,0 +1,22 @@
+﻿# -*- coding: utf-8 -*-
+
+{
+    'name': 'Testserver',
+    'version': '0.1',
+    'summary': 'Testserver',
+    'description': """
+Mark this server as a testserver!
+=================================
+So that it is not confused with the real one
+    """,
+    'author': 'Sunflower IT',
+    'website': 'http://sunflowerweb.nl',
+    'category': 'Specific Industry Applications',
+    'sequence': 0,
+    'depends': ['base', 'web'],
+    'data': [
+        'testserver.xml',
+    ],
+    'installable': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
