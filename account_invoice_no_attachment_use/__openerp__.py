e89d77939a507aa97dfca23c57d03f8f8f3eca77
Tom Blauwendraat <antiflu@gmail.com>
2020-05-20 15:20:18 +0200

    [ADD] port of account_invoice_no_attachment_use

---
+++ account_invoice_no_attachment_use/__openerp__.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': 'Invoice no attachment use',
+    'version': '9.0.1.0.0',
+    'summary': 'Disable attachment use flag for invoice report',
+    'author': 'Sunflower IT',  
+    'website': 'http://www.sunflowerweb.nl',
+    'category': 'Sales Management',
+    'depends': ['account'],
+    'data': [
+        'data/account_invoice_pdf_report.xml',
+    ],
+    'installable': True,
+    'active': False,
+}
+
